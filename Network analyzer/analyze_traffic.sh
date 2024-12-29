8a968fdb947203fbe9636dfda1f2b371482e93a4
Bahey1200022 <bahy2002@gmail.com>
2024-12-29 23:24:46 +0200

    m

--- Network analyzer/analyze_traffic.sh
+++ Network analyzer/analyze_traffic.sh
@@ -19,15 +19,23 @@ fi
 
 # get num of packets in pcap file
 #tshark -r traffic2.pcap | wc -l
-count=$(tshark -r $filename | wc -l)
+count=$(tshark -r "$filename" | wc -l)
 echo "Number of packets in the pcap file: $count"
 
 # get protocols in pcap file
 echo "Protocols in the pcap file:"
-tshark -r $filename -z io,phs -q | tr -s ' ' | cut -f 2 -d ' ' | tail -n +7 | head -n -1
+tshark -r "$filename" -z io,phs -q | tr -s ' ' | cut -f 2 -d ' ' | tail -n +7 | head -n -1
 
 
 # get number of packets for each protocol
-tshark -r $filename -q -z io,phs
+tshark -r "$filename" -q -z io,phs
 
 #get top 5 destination IP addresses
+
+echo "Top 5 destination IP addresses:"
+tshark -r "$filename" -T fields -e ip.dst | sort | uniq -c | sort -nr | head -n 5
+
+# get top 5 source IP addresses
+echo "Top 5 source IP addresses:"
+
+tshark -r "$filename"  -T fields -e ip.src | sort | uniq -c | sort -nr | head -n 5
---
+++ Network analyzer/readme.md
@@ -0,0 +1,14 @@
+
+pcap network traffic analyzer
+
+r1 : total packets count and protocols in the pcap file
+
+![alt text](image.png)
+
+r2 : packets per protocols 
+
+![alt text](image-1.png)
+
+r3 : ip's with most num of  destination packets and source packets 
+
+![alt text](image-2.png)
